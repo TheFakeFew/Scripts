62671020f9e7ee07133b8fd1f550c0a39989d3e2
Noah <108598167+TheFakeFew@users.noreply.github.com>
2023-03-08 18:59:53 -0500

    Update Neptunian.lua

--- Neptunian.lua
+++ Neptunian.lua
@@ -124,7 +124,7 @@ function EZConvert()
 				CoordinateFrame=CFrame.new();
 				CFrame=CFrame.new();	
 			}
-			local FakeMouse = NLS([[local me = game:service'Players'.localPlayer;
+			local FakeMouse = NLS([[task.wait() local me = game:service'Players'.localPlayer;
 local pg = me:FindFirstChildOfClass'PlayerGui'
 local mouse = me:GetMouse();
 local UIS = game:service'UserInputService'
