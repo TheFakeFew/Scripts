54de3e19bd400f56a1bbc338e37d697ab39a4605
Noah <108598167+TheFakeFew@users.noreply.github.com>
2023-04-02 21:13:32 -0400

    Update BeamVis.lua

--- BeamVis.lua
+++ BeamVis.lua
@@ -1,7 +1,9 @@
 if(not owner)then
 	getfenv().owner = script:FindFirstAncestorOfClass("Player") and script:FindFirstAncestorOfClass("Player") or game:GetService('Players'):GetPlayerFromCharacter(script:FindFirstAncestorOfClass("Model"))
 end
-if(game:GetService("RunService"):IsServer())then script.Parent = workspace end
+if(game:GetService("RunService"):IsServer())then script.Name = owner.Name.."BeamVis" end
+task.wait()
+script.Parent = workspace
 local ArtificialHB = Instance.new("BindableEvent", script)
 ArtificialHB.Name = "Heartbeat"
 
@@ -35,68 +37,75 @@ game:GetService("RunService").Heartbeat:connect(function(s, p)
 end)
 local visframes = {}
 local numberofframes = 100
-local partfold = Instance.new("Folder", workspace)
+local partfold = Instance.new("Folder", script)
 partfold.Name = "VisParts"..owner.Name
 local id = 0
 local vol = 1
 local pit = 1
 local tpos = 0
 local size = 4
-local b = Instance.new("Part",partfold)
+local b = Instance.new("Part")
 b.Name = "Base"
 b.Anchored = true
 b.CanCollide = false
 b.Size = Vector3.new(1,1,1)
 b.Position = Vector3.new(0,5,0)
 b.Transparency = 1
+b.Parent = partfold
 local beams = {}
 local beams2 = {}
 local visframes2 = {}
 for i = 1,numberofframes do
-	local a = Instance.new("Attachment",b)
+	local a = Instance.new("Attachment")
 	a.CFrame = CFrame.Angles(0,math.rad((i-1)*3.6),0)*CFrame.new(0,0,size)
 	if(visframes[i-1])then
-		local beam = Instance.new("Beam", a)
+		local beam = Instance.new("Beam")
 		beam.Attachment0 = visframes[i-1].self
 		beam.Attachment1 = a
 		beam.LightEmission = 1
 		beam.LightInfluence = 1
 		beam.Width0 = .3
 		beam.Width1 = .3
+		beam.Parent = a
 		beams[i-1] = beam
 	end
+	a.Parent = b
 	visframes[i]={self = a, OrigCF = a.CFrame}
 end
 for i = 1, numberofframes do
-	local a = Instance.new("Attachment",b)
+	local a = Instance.new("Attachment")
 	a.CFrame = CFrame.Angles(0,math.rad((i-1)*3.6),0)*CFrame.new(0,0,size)
 	if(visframes2[i-1])then
-		local beam = Instance.new("Beam", a)
+		local beam = Instance.new("Beam")
 		beam.Attachment0 = visframes[i-1].self
 		beam.Attachment1 = a
 		beam.LightEmission = 1
 		beam.LightInfluence = 1
 		beam.Width0 = .15
 		beam.Width1 = .15
+		beam.Parent = a
 		beams2[i-1] = beam
 	end
+	a.Parent = b
 	visframes2[i]={self = a, OrigCF = a.CFrame}
 end
-local beam = Instance.new("Beam", visframes[1].self)
+local beam = Instance.new("Beam")
 beam.Attachment0 = visframes[#visframes].self
 beam.Attachment1 = visframes[1].self
 beam.LightEmission = 1
 beam.LightInfluence = 1
 beam.Width0 = .3
 beam.Width1 = .3
+beam.Parent = visframes[1].self
 beams[#beams+1] = beam
-local beam = Instance.new("Beam", visframes2[1].self)
+local beam = Instance.new("Beam")
 beam.Attachment0 = visframes[#visframes].self
 beam.Attachment1 = visframes2[1].self
 beam.LightEmission = 1
 beam.LightInfluence = 1
 beam.Width0 = .15
 beam.Width1 = .15
+beam.Parent = visframes2[1].self
 beams2[#beams+1] = beam
 local mus = Instance.new("Sound", b)
 mus.SoundId = "rbxassetid://"..id
@@ -120,10 +129,10 @@ if(game:GetService("RunService"):IsServer())then
 		getfenv().owner = script:FindFirstAncestorOfClass("Player") and script:FindFirstAncestorOfClass("Player") or game:GetService('Players'):GetPlayerFromCharacter(script:FindFirstAncestorOfClass("Model"))
 	end
 	local rem = game:GetService('ReplicatedStorage'):WaitForChild(owner.Name.."FunnyVis")
-	local Music = workspace:WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
+	local Music = workspace:WaitForChild(owner.Name.."BeamVis"):WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
 	local hb
 	hb = game:GetService('RunService').Heartbeat:Connect(function()
-		Music = workspace:WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
+		Music = workspace:WaitForChild(owner.Name.."BeamVis"):WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
 		rem = game:GetService('ReplicatedStorage'):WaitForChild(owner.Name.."FunnyVis")
 		if(not rem or not rem:IsDescendantOf(game:GetService('ReplicatedStorage')))then
 			hb:Disconnect()
@@ -134,10 +143,10 @@ if(game:GetService("RunService"):IsServer())then
 		rem:FireServer(Music.PlaybackLoudness)
 	end)]], owner.Character)
 else
-	local Music = workspace:WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
+	local Music = script:WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
 	local hb
 	hb = game:GetService('RunService').Heartbeat:Connect(function()
-		Music = workspace:WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
+		Music = script:WaitForChild("VisParts"..owner.Name):WaitForChild("Base"):WaitForChild("Music")
 		loudness = Music.PlaybackLoudness
 	end)
 end
@@ -176,9 +185,7 @@ ArtificialHB.Event:Connect(function()
 		params.FilterDescendantsInstances = {owner.Character, partfold}
 		local ray = workspace:Raycast(rootpart.Position, Vector3.new(0, -99999, 0), params)
 		if(ray)then
-			game:GetService("TweenService"):Create(b, TweenInfo.new(.5), {
-				CFrame = CFrame.new(ray.Position, ray.Position + ray.Normal) * CFrame.Angles(math.rad(-90), math.rad((tick()*10)%360), 0)
-			}):Play()
+			b.CFrame = b.CFrame:Lerp(CFrame.new(ray.Position, ray.Position + ray.Normal) * CFrame.Angles(math.rad(-90), math.rad((tick()*10)%360), 0), .1)
 		end
 	end
 	for i,v in next, visframes do
@@ -189,9 +196,7 @@ ArtificialHB.Event:Connect(function()
 		local col = math.clamp(loudness/400*(i/(#visframes*math.random(1,2))), .5, 1)
 		local beam = beams[i-1]
 		local beam2 = beams2[i-1]
-		game:GetService("TweenService"):Create(v.self, TweenInfo.new(.5), {
-			CFrame = v.OrigCF*CFrame.new(0,(noise > 0 and noise or -noise)*(loudness/50),0)
-		}):Play()
+		v.self.CFrame = v.self.CFrame:Lerp(v.OrigCF*CFrame.new(0,(noise > 0 and noise or -noise)*(loudness/50),0), .1)
 		if(beam)then
 			beam.Color = ColorSequence.new(beam.Color.Keypoints[1].Value:Lerp(Color3.fromHSV(tick()%1, col, col), .1))
 			beam2.Color = ColorSequence.new(beam2.Color.Keypoints[1].Value:Lerp(Color3.fromHSV(tick()%1, col, col), .1))
