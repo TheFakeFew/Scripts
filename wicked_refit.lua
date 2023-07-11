aad00310a03c987c0a86ec1b618d416a3f4d6f6c
Noah <108598167+TheFakeFew@users.noreply.github.com>
2023-07-11 19:39:13 -0400

    Update wicked_refit.lua

--- wicked_refit.lua
+++ wicked_refit.lua
@@ -558,7 +558,7 @@ end
 function respawn()
 	pcall(game.Destroy, char)
 	pcall(game.Destroy, owner.Character)
-	
+
 	oldcframes = {
 		CHARACTER = {
 			Character = CFRAMES.CHARACTER.Character.Y <= workspace.FallenPartsDestroyHeight + 20 and relocate() or CFRAMES.CHARACTER.Character,
