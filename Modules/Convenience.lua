8edd9a92ae91817c5ecd52d0897291aefd4fd64e
Noah <108598167+TheFakeFew@users.noreply.github.com>
2023-08-16 03:12:42 -0400

    Update Convenience.lua

--- Modules/Convenience.lua
+++ Modules/Convenience.lua
@@ -242,7 +242,7 @@ function module.EZConvert()
 				table.insert(wrapped, index, wrappedObjects[unwrap(v)] or sandbox(v))
 			end
 		end
-		return table.unpack(wrapped, 0, index)
+		return table.unpack(wrapped, 1, index)
 	end
 
 	function wrapfunction(f)
