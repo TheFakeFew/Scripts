da2e6eeb7872ceec9d03dcf20c258aa7591ec25c
Noah <108598167+TheFakeFew@users.noreply.github.com>
2024-11-02 02:49:00 -0400

    Update fractal.lua

--- ConsolePackages/fractal.lua
+++ ConsolePackages/fractal.lua
@@ -4,9 +4,9 @@ return {
             iterations = 5
         end
         iterations = tonumber(iterations)
-        
+
         local done = false
-        
+
         local function gracefulkeybind(key, ctrl, func)
             bindtokey(key, ctrl, func)
             table.insert(threadinstances, {
@@ -16,10 +16,10 @@ return {
                 end,
             })
         end
-        
+
         local fractalobjects = {}
         local listlayout = console.SurfaceGui.ParentFrame.UIListLayout
-        
+
         gracefulkeybind("c", true, function()
             for i = #fractalobjects, 1, -1 do
                 if(i%200 == 0)then
@@ -28,11 +28,11 @@ return {
                 pcall(game.Destroy, fractalobjects[i])
             end
             table.clear(fractalobjects)
-            
+
             listlayout.Parent = console.SurfaceGui.ParentFrame
             done = true
         end)
-        
+
         local function createFrame(size, position, color)
             local frame = Instance.new("Frame")
             frame.Size = size
@@ -43,15 +43,15 @@ return {
             table.insert(fractalobjects, frame)
             return frame
         end
-        
+
         local index = 0
         local function drawFractalTree(startPos, length, width, angle, depth, istrunk, last)
             index += 1
             if depth <= 0 then return end
 
-            local trunk = createFrame(UDim2.new(0, width, 0, length), startPos, Color3.fromHSV(depth/iterations, 1, 1))
+            local trunk = createFrame(UDim2.new(0, math.ceil(width), 0, math.ceil(length)), startPos, Color3.fromHSV(depth/iterations, 1, 1))
             trunk.AnchorPoint = Vector2.new(.5, 1)
-            
+
             if(istrunk)then
                 trunk.AnchorPoint = Vector2.new(.5, 1)
                 trunk.Rotation = 0
@@ -59,17 +59,17 @@ return {
             else
                 trunk.AnchorPoint = Vector2.new(.5 * (angle > 0 and 1 or -1), 1)
                 trunk.Rotation = angle
-                trunk.Position = UDim2.fromScale(2 * (angle > 0 and 1 or -1), 0, 0, 0)
+                trunk.Position = UDim2.fromScale(2 * (angle > 0 and 1 or -0.75), 0, 0, 0)
                 trunk.Parent = last
             end
-            
+
             if(index%150 == 0)then
                 task.wait()
             end
             drawFractalTree(startPos, length*.7, width * .7, -angle, depth - 1, false, trunk)
             drawFractalTree(startPos, length*.7, width * .7, angle, depth - 1, false, trunk)
         end
-        
+
         local function drawSierpinskiTriangle(p1, p2, p3, depth)
             index += 1
             if depth == 0 then
@@ -88,7 +88,7 @@ return {
             local mid1 = Vector2.new((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2)
             local mid2 = Vector2.new((p2.X + p3.X) / 2, (p2.Y + p3.Y) / 2)
             local mid3 = Vector2.new((p1.X + p3.X) / 2, (p1.Y + p3.Y) / 2)
-            
+
             if(index%100 == 0)then
                 task.wait()
             end
@@ -96,7 +96,7 @@ return {
             drawSierpinskiTriangle(mid1, p2, mid2, depth - 1)
             drawSierpinskiTriangle(mid3, mid2, p3, depth - 1)
         end
-        
+
         local function drawMandelbrot(width, height, size)
             for x = 1, width do
                 for y = 1, height do
@@ -110,7 +110,7 @@ return {
                         zy, zx = 2 * zx * zy + cY, tmp
                         i = i + 1
                     end
-                    
+
                     if(i > 3 and i < iterations)then
                         local colorValue = Color3.fromHSV(i/iterations, 1, 1)
                         local frame = createFrame(UDim2.new(0, size, 0, size), UDim2.new(0,x*size, 0, y*size), colorValue)
@@ -119,7 +119,7 @@ return {
                 task.wait()
             end
         end
-        
+
         if(type == "tree")then
             listlayout.Parent = nil
             clearoutput()
@@ -143,7 +143,7 @@ return {
             errortext("invalid fractal type. did you mean? (tree, triangle, mandelbrot)")
             return
         end
-        
+
         repeat task.wait() until done
     end,
     description = "generates fractals"
