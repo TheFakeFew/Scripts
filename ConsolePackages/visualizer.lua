b7bbff5df7b8305bdc4b865b7a2009196dced98e
Noah <108598167+TheFakeFew@users.noreply.github.com>
2024-11-01 23:33:05 -0400

    Update visualizer.lua

--- ConsolePackages/visualizer.lua
+++ ConsolePackages/visualizer.lua
@@ -36,7 +36,7 @@ return {
 
         clearoutput()
         local vistext = addText("loading...")
-        vistext.TextSize = 54
+        vistext.TextSize = 40
         vistext.TextWrapped = false
         addText("[^Z] Volume Up  [^X] Volume Down  [^C] Exit")
         
