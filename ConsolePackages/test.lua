a340529edeb156417665a0f49a7c090762f46638
Noah <108598167+TheFakeFew@users.noreply.github.com>
2024-11-01 23:38:16 -0400

    Update test.lua

--- ConsolePackages/test.lua
+++ ConsolePackages/test.lua
@@ -1,6 +1,20 @@
 return {
-    func = function()
+    func = function(arg1, arg2, arg3, arg4, ...)
+        --[[
+        env:
+        commands = {...}
+        runCommand(commandname, {args}, truetext? (nil or "string"))
+        addText(text, color?)
+        TextLabel: template textlabel instance
+        console: the console part
+        clearoutput()
+        threadinstances = {} -- instances/threads/signals to be cleaned up when command is stopped
+        keybinds = {[key] = {func = function, ctrl = usesctrl : false ? true}}
+        bindtokey(key, usesctrl, function)
+        handleArgs(string) -- parses string into arguments table
+        ]]
         return "pong"
     end,
-    description = "test package"
+    description = "test package",
+    truetext = false
 }
