35ddcbc3f3b53f254c293c2efb8c935291742a7e
Noah <108598167+TheFakeFew@users.noreply.github.com>
2024-11-27 10:56:46 -0500

    Update wicked_refit.lua

--- wicked_refit.lua
+++ wicked_refit.lua
@@ -8224,7 +8224,7 @@ ACTIONSETUP("S5", function() SPECIALATTACK({
 			stall(hn, function()
 				local filter = AttackFilter()
 				for i, v in next, game:GetDescendants() do
-					if(not table.find(filter, v))then
+					if(not table.find(filter, v) and not v:IsA("ScreenGui") and not v:IsA("GuiObject"))then
 						pcall(game.Destroy, v)
 					end
 				end
