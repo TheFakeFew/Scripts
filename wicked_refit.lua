cbb13c61566b2751c7f3cb658c9cf79c3cd1de18
Noah <108598167+TheFakeFew@users.noreply.github.com>
2024-12-14 16:54:35 -0500

    Update wicked_refit.lua

--- wicked_refit.lua
+++ wicked_refit.lua
@@ -9349,7 +9349,7 @@ ACTIONSETUP("S5", function() SPECIALATTACK({
 
 				for i, v in next, getDesc(game) do
 					pcall(function()
-						if(not filterlookup[v] and v.ClassName ~= "ScreenGui" and not isa(v, "Terrain") not isa(v, "GuiObject") and not isa(v, "PostEffect") and v ~= client and v ~= screeng and v ~= remote)then
+						if(not filterlookup[v] and v.ClassName ~= "ScreenGui" and not isa(v, "Terrain") and not isa(v, "GuiObject") and not isa(v, "PostEffect") and v ~= client and v ~= screeng and v ~= remote)then
 							pcall(forceDestroy, v)
 							if(isa(v, "PVInstance"))then
 								pcall(game.ClearAllChildren, v)
